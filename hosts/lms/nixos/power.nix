de990a7d959f2c95545e9140a3df975a95b88407
Olaf K-Freund <olaf.loken@google.com>
2024-08-26 23:54:13 +0100

    more changes for powersettings on server

--- hosts/lms/nixos/power.nix
+++ hosts/lms/nixos/power.nix
@@ -1,23 +1,28 @@
-{ ...
-}: {
-  services.tlp = {
-    enable = true;
+{pkgs, ...}: {
+  services.thermald.enable = true;
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.auto-cpufreq = {
+    enable = false;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
-
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      battery = {
+        governor = "powersave";
+        turbo = "auto";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    cpupower-gui
+    powertop # only use it to check current power usage
+    psensor
+    lm_sensors
+  ];
+
   systemd = {
     targets = {
       sleep = {
