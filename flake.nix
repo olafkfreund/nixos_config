8e7278beeeab75ec0fc9944fad1863847104d6d1
Olaf K-Freund <olaf.loken@google.com>
2024-12-06 11:38:16 +0000

    update flake.lock and flake.nix to switch home-manager to release-24.11 and use nixpkgs instead of nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -40,8 +40,8 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     sops-nix = {
