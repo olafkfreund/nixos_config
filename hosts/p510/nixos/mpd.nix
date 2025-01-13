aa28c7cc347450e80da8dc9eda4d3b42d10ae14c
olafkfreund <olaf.loken@gmail.com>
2025-01-13 18:02:18 +0000

    feat: move XDG_RUNTIME_DIR to systemd service configuration for MPD

--- hosts/p510/nixos/mpd.nix
+++ hosts/p510/nixos/mpd.nix
@@ -7,9 +7,6 @@
     enable = true;
     musicDirectory = "/mnt/media/Media/Music";
     user = "olafkfreund";
-    environment = {
-      XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.olafkfreund.uid}";
-    };
     extraConfig = ''
       audio_output {
       type "pipewire"
@@ -20,5 +17,8 @@
     # Optional:
     network.listenAddress = "any"; # if you want to allow non-localhost connections
     network.startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
+  };
+  systemd.services.mpd.environment = {
+    XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.olafkfreund.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
   };
 }
