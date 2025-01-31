06c42017e57032f3fb174ee756a171c9657f3437
olafkfreund <olaf.loken@gmail.com>
2025-01-31 11:39:41 +0000

    fix: update boot and nvidia configurations for kernel package adjustments and driver version changes

--- hosts/razer/nixos/nvidia.nix
+++ hosts/razer/nixos/nvidia.nix
@@ -10,14 +10,7 @@
     nvidiaPersistenced = true;
     open = true;
     nvidiaSettings = false;
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.86.16"; # use new 570 drivers
-      sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
-      openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
-      settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
-      usePersistenced = false;
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   hardware.nvidia.prime = {
