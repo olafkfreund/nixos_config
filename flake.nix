928e809d9ba3355d61c1c71018d58c12a62789e2
olafkfreund <olaf.loken@gmail.com>
2025-02-10 09:51:45 +0000

    feat: update flake.lock and remove microvm configuration from flake.nix

--- flake.nix
+++ flake.nix
@@ -53,11 +53,6 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
 
-    microvm = {
-      url = github:astro/microvm.nix;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -121,7 +116,6 @@
     spicetify-nix,
     home-manager,
     stylix,
-    microvm,
     nix-index-database,
     zjstatus,
     ...
