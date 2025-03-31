55ab9dfabfe22d9de5c502887f9c0386ba3e6738
olafkfreund <olaf.loken@gmail.com>
2025-03-31 12:51:37 +0100

    fix: update flake.lock and remove commented theme links in nixcord configuration

--- home/chat/nixcord.nix
+++ home/chat/nixcord.nix
@@ -4,9 +4,6 @@
     quickCss = "./gruvb-dark.theme.css"; # path to your quickCSS file
     config = {
       useQuickCss = true; # use out quickCSS
-      # themeLinks = [
-      #   "https://refact0r.github.io/system24/theme/gruvbox-material.theme.css"
-      # ];
       frameless = true; # set some Vencord options
       plugins = {
         hideAttachments.enable = true; # Enable a Vencord plugin
