0201669f59f2dc9a8abcfd59235d9442f3a9af7f
olafkfreund <olaf.loken@gmail.com>
2025-05-09 03:54:30 +0100

    fix: comment out unused kernel and SSD tweak modules in configuration

--- modules/laptops.nix
+++ modules/laptops.nix
@@ -1,13 +1,10 @@
-{ ... }: {
+{...}: {
   imports = [
-    ./system-tweaks/storage-tweaks/SSD/SSD-tweak.nix
-    ./system-tweaks/kernel-tweaks/64GB-SYSTEM/64gb-system.nix
+    # ./system-tweaks/storage-tweaks/SSD/SSD-tweak.nix
+    # ./system-tweaks/kernel-tweaks/64GB-SYSTEM/64gb-system.nix
     ./laptop-related/earlyoom.nix
     ./laptop-related/zram.nix
     ./hardware/openrazer.nix
     ./laptop-related/udev.nix
   ];
-
-
 }
-
