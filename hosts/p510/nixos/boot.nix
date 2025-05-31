484a50d1122eb266f2a6d19ff6582dc105601c2e
olafkfreund <olaf.loken@gmail.com>
2025-05-31 13:03:58 +0100

    fix: Revert kernelPackages to stable version for compatibility

--- hosts/p510/nixos/boot.nix
+++ hosts/p510/nixos/boot.nix
@@ -7,7 +7,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
   boot.plymouth.enable = true;
 
   # Xeon E5-2698 v4 Optimizations
