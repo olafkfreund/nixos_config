64fea604151fa62d6d7509dd7afe4398a995579c
Olaf K-Freund <olaf.loken@google.com>
2025-10-31 11:57:43 +0000

    fix: temporarily disable Secure Boot on Razer due to lanzaboote v0.4.1 Rust build issues

--- hosts/razer/configuration.nix
+++ hosts/razer/configuration.nix
@@ -18,7 +18,7 @@ in
       ./nixos/screens.nix
       ./nixos/power.nix
       ./nixos/boot.nix
-      ./nixos/secure-boot.nix # Secure Boot enabled with lanzaboote
+      # ./nixos/secure-boot.nix # Secure Boot enabled with lanzaboote
       ./nixos/nvidia.nix
       ../common/nixos/i18n.nix
       ../common/nixos/hosts.nix
