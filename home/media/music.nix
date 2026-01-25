c47ff8344adbdb26d0895d1746126f8ea00f0779
olafkfreund <olaf.loken@gmail.com>
2026-01-25 18:20:22 +0000

    chore(music): remove alternative Spotify clients (#181)
    Remove redundant Spotify clients and tools to streamline music player
    configuration, keeping only the official Spotify client.
    
    Packages removed:
    - ncspot (TUI Spotify client)
    - spotify-player (Terminal Spotify player)
    - sptlrx (Spotify lyrics viewer)
    - spicetify-cli (Spotify customization tool)
    
    Package retained:
    - spotify (Official Spotify client)
    
    Closes #181

--- home/media/music.nix
+++ home/media/music.nix
@@ -1,19 +1,15 @@
 { pkgs, ... }: {
   home.packages = [
-    pkgs.spotify # Re-enabled while Spicetify is temporarily disabled (issue #13)
-    pkgs.ncspot # Spotify
+    pkgs.spotify # Official Spotify client
     pkgs.plexamp # Plex
     pkgs.vlc # video player
     pkgs.cava # audio visualizer
-    pkgs.spotify-player # Spotify
-    pkgs.sptlrx # Spotify Lyrics
     pkgs.castero # Podcasts - Re-enabled: Python 3.13.9 compatibility fixed (issue #35 closed)
     pkgs.gnome-podcasts # Podcasts
     pkgs.hypnotix
     pkgs.wiremix # Music streaming
     pkgs.parabolic
     pkgs.musicpod
-    pkgs.spicetify-cli
     pkgs.playerctl # Required dependency for our script
     (pkgs.callPackage ../../pkgs/mpris-album-art { }) # Our album art script
   ];
